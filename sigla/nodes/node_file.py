4c6fbca577783c833fd5927b556a50c8fdbb0ced
MG Santos <mauro.goncalo@gmail.com>
2021-07-13 23:06:28 +0200

    Add TODOs

--- sigla/nodes/node_file.py
+++ sigla/nodes/node_file.py
@@ -3,6 +3,17 @@ from pathlib import Path
 from sigla.nodes.node import Node
 from sigla.utils.helpers import ensure_dirs
 
+# TODO
+# <models>
+#   <model name="User">
+#     ...
+#   </model>
+# </models>
+# <forms>
+#   <user_form model="User" /or/ model-query="models model[name='User']" /or/ $model="User">
+#     ...
+#   </user_form>
+# </forms>
 
 class NodeFile(Node):
     def __init__(self, tag, *args, **kwargs):
@@ -20,15 +31,17 @@ class NodeFile(Node):
         return self
 
     def validate_to_attribute(self):
+        # TODO convert this to validation mechanism
         if "to" not in self.data.attributes.keys():
             raise AttributeError(
                 "You need to provide the propriety `to` with a filepath on "
                 "the element <file> to save the results to"
             )
 
-    def __eq__(self, o: any) -> bool:
-        if type(self) == self.__class__ and type(o) == self.__class__:
-            if self.content == o.content and self.to == o.path:
+    def __eq__(self, other: any) -> bool:
+        # TODO apply this logic to other __eq__ methods
+        if type(self) == self.__class__ and type(other) == self.__class__:
+            if self.content == other.content and self.to == other.to:
                 return True
         return False
 
