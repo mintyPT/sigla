9b09e67f669a301accec9a3ffe2b7c3e39b67b05
mg santos <mauro.goncalo@gmail.com>
2021-07-30 23:44:29 +0200

    filter_collector

--- sigla/cli/filter_collector.py
+++ sigla/cli/filter_collector.py
@@ -1,16 +1,18 @@
-from typing import Callable, Dict
+from typing import Callable, Dict, Any
 
 
 class FilterCollector:
+    filters: Dict[str, Callable[[Any], Any]]
+
     def __init__(self) -> None:
         self.filters = {}
 
-    def register(self, name: str) -> Callable:
-        def wraps(f: Callable) -> Callable:
+    def register(self, name: str) -> Callable[[Any], Any]:
+        def wraps(f: Callable[[Any], Any]) -> Callable[[Any], Any]:
             self.filters[name] = f
             return f
 
         return wraps
 
-    def get_filters(self) -> Dict:
+    def get_filters(self) -> Dict[str, Callable[[Any], Any]]:
         return self.filters
