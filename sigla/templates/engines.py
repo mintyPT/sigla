404e37d07a568e4a2421f3fc77e8f97e7c636aa1
mauro santos <mauro@scandit.com>
2022-10-25 14:53:19 +0200

    fix some types

--- sigla/templates/engines.py
+++ sigla/templates/engines.py
@@ -1,17 +1,18 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
-from jinja2 import Environment, BaseLoader
+from jinja2 import Environment, BaseLoader, Template
 
 
 class TemplateEngineABC(ABC):
     @abstractmethod
-    def render(self, template: str, filters: dict, **kwargs: any) -> str:
+    def render(self, template: str, filters: dict, **kwargs: Any) -> str:
         pass
 
 
 class JinjaEngine(TemplateEngineABC):
-    def render(self, template: str, filters: dict, **kwargs: any) -> str:
+    def render(self, template: str, filters: dict, **kwargs: Any) -> str:
         env = Environment(loader=BaseLoader())
         env.filters.update(filters)
-        template = env.from_string(template)
-        return template.render(**kwargs)
+        jinja_tpl: Template = env.from_string(template)
+        return jinja_tpl.render(**kwargs)
